6a68e58ffaa1420fb855810a1c65c1238ff926bb
phurinut <phurinutzaza87@gmail.com>
2026-03-16 22:14:56 +0700

    Update API URLs to Railway

--- engse207-final-lab2-[675432100708]-[675432100229]/frontend/config.js
+++ engse207-final-lab2-[675432100708]-[675432100229]/frontend/config.js
@@ -1,9 +1,9 @@
 // frontend/config.js
 window.APP_CONFIG = {
   // 💻 สำหรับเทสในเครื่อง (Local)
-  AUTH_URL: 'http://localhost:3001',
-  TASK_URL: 'http://localhost:3002',
-  USER_URL: 'http://localhost:3003'
+  AUTH_URL: 'https://auth-service-production-4c89.up.railway.app',
+  TASK_URL: 'https://task-service-production-6814.up.railway.app',
+  USER_URL: 'https://user-service-production-5ad5.up.railway.app'
 
   /* ☁️ เมื่อจะขึ้น Railway ให้สลับมาใช้ด้านล่างนี้:
   AUTH_URL: 'https://auth-service-production-XXXX.up.railway.app',
